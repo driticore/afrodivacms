716ffd7a0dd9ec90880de34d7c641fba4dfcd873
Akadinso Osuagwu <89935388+driticore@users.noreply.github.com>
2025-01-11 11:59:17 +0200

    Update database.php

--- includes/database.php
+++ includes/database.php
@@ -1,6 +1,6 @@
 <?php
 
-$connect = mysqli_connect("mysql.db.mdbgo.com","driticore_afrodivacms","Dhl@min1","driticore_afrodivacms");
+$connect = mysqli_connect("mysql.db.mdbgo.com","driticore_afrodivacms","Akadinso.ca0","driticore_afrodivacms");
 
 if (mysqli_connect_errno()) {
     exit("Failed to connect to MYSQL: " . mysqli_connect_error());
